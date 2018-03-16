90d152f102c523133919866e727d70ba84068658
thaxor <luke@thaxor.com>
2018-03-15 18:20:37 -0700

    added db seeding

--- Assessment/Startup.cs
+++ Assessment/Startup.cs
@@ -40,7 +40,7 @@ namespace Assessment
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure( IApplicationBuilder app, IHostingEnvironment env, RoleManager<IdentityRole> roleManager)
+        public void Configure( IApplicationBuilder app, IHostingEnvironment env, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             if (env.IsDevelopment())
             {
