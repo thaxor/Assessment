71409ec41cf5241ea147f477bd257ee2f259916b
thaxor <lukehansen@gmail.com>
2018-03-16 18:32:29 -0700

    added start of user role management view

---
+++ Assessment/Controllers/RoleManagementController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assessment.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Assessment.Controllers
+{
+    public class RoleManagementController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger _logger;
+
+        public RoleManagementController(
+          UserManager<ApplicationUser> userManager,
+          ILogger<ManageController> logger)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            var users = _userManager.Users.ToList();
+
+            return View(users);
+        }
+    }
+}
