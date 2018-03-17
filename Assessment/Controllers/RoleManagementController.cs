0f734865b01f201a96c8c47f7ae7f477b3a89d71
thaxor <lukehansen@gmail.com>
2018-03-16 19:25:47 -0700

    edit user roles page created

--- Assessment/Controllers/RoleManagementController.cs
+++ Assessment/Controllers/RoleManagementController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Assessment.Models;
+using Assessment.Models.RoleManagementViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -11,13 +12,16 @@ namespace Assessment.Controllers
 {
     public class RoleManagementController : Controller
     {
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger _logger;
 
         public RoleManagementController(
+            RoleManager<IdentityRole> roleManager,
           UserManager<ApplicationUser> userManager,
           ILogger<ManageController> logger)
         {
+            _roleManager = roleManager;
             _userManager = userManager;
             _logger = logger;
         }
@@ -33,5 +37,22 @@ namespace Assessment.Controllers
 
             return View(users);
         }
+
+        
+        public async Task<ActionResult> EditUserRoles([FromQuery] string userId)
+        {
+            var user = _userManager.Users.First(u => u.Id.Equals(userId));
+            
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var model = new EditUserRolesViewModel();
+
+            model.User = user;
+            model.UserRoles = userRoles.ToList();
+            model.ApplicationRoles = _roleManager.Roles.ToList().Select(x => x.Name).ToList();
+
+            return View(model);
+        }
+
     }
 }
