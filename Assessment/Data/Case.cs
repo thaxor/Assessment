07aa9cb1cee21ffeae5be098f3d950903b6940f3
Luke Hansen <lukehansen@gmail.com>
2018-03-17 11:37:08 -0700

    added case model, created migration

---
+++ Assessment/Data/Case.cs
@@ -0,0 +1,11 @@
+﻿namespace Assessment.Data
+{
+    public class Case
+    {
+        public int CaseId { get; set; }
+        public string Message { get; set; }
+        public string WorkerId { get; set; }
+        public string ReviewerId { get; set; }
+        public string ApproverId { get; set; }
+    }
+}
