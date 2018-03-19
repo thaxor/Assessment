ecf79f4a4a8a28e00e458a33f00a2a4ba3c13f70
Luke Hansen <lukehansen@gmail.com>
2018-03-18 18:45:00 -0700

    add dapper dependency for sproc access, add user case load model for report display, add report display to index

--- Assessment/Controllers/HomeController.cs
+++ Assessment/Controllers/HomeController.cs
@@ -5,14 +5,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Assessment.Models;
+using Assessment.Data;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
 
 namespace Assessment.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ApplicationDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            IEnumerable<UserCaseLoad> result;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                result = connection.Query<UserCaseLoad>("spUserCaseLoadReport", new { }, commandType: System.Data.CommandType.StoredProcedure);
+            };
+
+            return View(result);
         }
 
         public IActionResult About()
---
+++ Assessment/Data/UserCaseLoad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assessment.Data
+{
+    public class UserCaseLoad
+    {
+        public string UserName { get; set; }
+        public int WorkerCases { get; set; }
+        public int ReviewedCases { get; set; }
+        public int ApprovedCases { get; set; }
+    }
+}
